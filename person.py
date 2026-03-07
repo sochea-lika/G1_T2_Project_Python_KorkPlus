8a165b683b9440bb0407dfbb3276ef9a9cfeaed0
SaoSochealika <sochealikasao@gmail.com>
2026-03-07 13:41:06 +0700

    update user login with ID

--- person.py
+++ person.py
@@ -3,13 +3,9 @@ class Person:
     Base class representing a person.
     Demonstrates inheritance and encapsulation.
     """
-    def __init__(self, name, email, password):
+    def __init__(self, name, password):
         self.name = name
-        self.email = email
         self._password = password  
-
-    def get_email(self):
-        return self.email
 
     def set_password(self, new_password):
         self._password = new_password
